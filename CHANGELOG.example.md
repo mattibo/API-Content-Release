ef4df5a3090f607f747198aa5ef06b5e1220b6b1
Matti Borchers <matti_borchers@gmx.de>
2021-05-22 10:32:07 +0200

    create example changelog

---
+++ CHANGELOG.example.md
@@ -0,0 +1,20 @@
+
+
+
+
+# KW x
+
+## Service
+
+### Hinzugefügt
+
+- ABD
+- DEF
+
+### Gelöscht
+
+- XYZ
+
+|Service-ID|Attribut|Alter Wert|Neuer Wert|
+| :---: | :---: | :---: | :---: |
+|1|name|ABC|DEF|
---
+++ create_example_changelog.py
@@ -0,0 +1,24 @@
+# create example changelog file (markdown format)
+
+from mdutils.mdutils import MdUtils
+
+chlog_file = MdUtils(file_name="CHANGELOG.example.md")
+
+chlog_file.new_header(level=1, title='KW x')
+chlog_file.new_header(level=2, title="Service")
+chlog_file.new_header(level=3, title="Hinzugefügt")
+
+services_added = ["ABD", "DEF"]
+
+chlog_file.new_list(services_added)
+
+chlog_file.new_header(level=3, title="Gelöscht")
+services_removed = ["XYZ"]
+chlog_file.new_list(services_removed)
+
+services_modified = ["Service-ID", "Attribut", "Alter Wert", "Neuer Wert"]
+services_modified.extend(["1", "name", "ABC", "DEF"])
+chlog_file.new_table(columns=4, rows=2, text=services_modified, text_align='center')
+
+chlog_file.create_md_file()
+
